dac9374e6c0b0cd094456ae9380304f65031b186
Dhiwakar <fastpointcab@gmail.com>
2025-11-02 09:13:45 +0530

    Fix vercel routing and node version

--- api/server.js
+++ api/server.js
@@ -59,12 +59,12 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-// --- Export for Vercel ---
-export default app;
+
 // --- Local run ---
 if (process.env.NODE_ENV !== "production") {
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 }
 
-
+// --- Export for Vercel ---
+export default app;
